c24410b4cead26f82c39d41311591cfd6bee0762
gohcassa <71555078+gohcassa@users.noreply.github.com>
2020-09-19 21:00:03 +0800

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,18 +1,8 @@
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
-
 import pandas as pd
 import tkinter.filedialog 
 # Get file path 
 path = tkinter.filedialog.askopenfilename()
-
-
-# path = "/Users/cassandriagoh/Desktop/Python/Kaggle and applications/PLAYING.csv"
 
 data = pd.read_csv(path)
 # data = data.set_index('respid')
@@ -26,7 +16,6 @@ for col in data:
         if row == 1:
             product_codes.append(value)
             users.append(col)
-            # print (col, data.iloc[i, 0])
 user_purchases = pd.DataFrame({"product_code": product_codes, "users": users})
 
 export = tkinter.filedialog.asksaveasfilename(defaultextension = '.csv')
